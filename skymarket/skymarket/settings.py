e5bde8c5aea9a6990c1c03dcc85727b2978dd0bf
plamer <plamer88@yandex.ru>
2023-03-18 16:52:46 +0300

    change settings

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -187,6 +187,7 @@ CORS_ALLOWED_ORIGINS = ['http://skyrent-server.ru:3000',
                         'http://localhost:3000',
                         'http://localhost:8000',
                         'http://127.0.0.1', ]
+CORS_ALLOWED_CREDENTIALS = False
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
